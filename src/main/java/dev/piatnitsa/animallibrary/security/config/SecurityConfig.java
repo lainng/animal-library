e36f7533ab1e98f34492644bee36b8d854740084
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-08 12:53:29 +0300

    Update security configuration

--- src/main/java/dev/piatnitsa/animallibrary/security/config/SecurityConfig.java
+++ src/main/java/dev/piatnitsa/animallibrary/security/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package dev.piatnitsa.animallibrary.security.config;
 
+import dev.piatnitsa.animallibrary.security.ForbiddenHandler;
+import dev.piatnitsa.animallibrary.security.UnauthorizedEntryPoint;
+import dev.piatnitsa.animallibrary.security.jwt.TokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,10 +13,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenFilter tokenFilter;
+    private final ForbiddenHandler forbiddenHandler;
+    private final UnauthorizedEntryPoint unauthorizedEntryPoint;
+
+    @Autowired
+    public SecurityConfig(TokenFilter tokenFilter,
+                          ForbiddenHandler forbiddenHandler,
+                          UnauthorizedEntryPoint unauthorizedEntryPoint) {
+        this.tokenFilter = tokenFilter;
+        this.forbiddenHandler = forbiddenHandler;
+        this.unauthorizedEntryPoint = unauthorizedEntryPoint;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -24,12 +41,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/register", "/login", "/name-availability").permitAll()
                     .antMatchers("/animal/*").authenticated()
                 .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .failureHandler()
-                    .successHandler();
-
-        //todo jwtFilter and exception handlers
+                .exceptionHandling()
+                    .accessDeniedHandler(forbiddenHandler)
+                    .authenticationEntryPoint(unauthorizedEntryPoint)
+                .and()
+                .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
