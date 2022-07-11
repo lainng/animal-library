76ad330d72f92b4a9eb147575c47c6f7698c9032
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-11 21:10:43 +0300

    Add language config

---
+++ src/main/java/dev/piatnitsa/animallibrary/i18n/LanguageConfig.java
@@ -0,0 +1,42 @@
+package dev.piatnitsa.animallibrary.i18n;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * This class contains Spring configuration for project to work with locale.
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
+@Configuration
+public class LanguageConfig extends AcceptHeaderLocaleResolver {
+    private final List<Locale> LOCALES = Arrays.asList(
+            new Locale("en"),
+            new Locale("ru")
+    );
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String headerLang = request.getHeader("Accept-Language");
+        return headerLang == null || headerLang.isEmpty()
+                ? Locale.getDefault()
+                : Locale.lookup(Locale.LanguageRange.parse(headerLang), LOCALES);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+}
