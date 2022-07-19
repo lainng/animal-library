4cf590e56f914aa3aa3c12366b43449b0cb220c5
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-19 21:37:16 +0300

    Added ID validation

--- src/main/java/dev/piatnitsa/animallibrary/service/AbstractService.java
+++ src/main/java/dev/piatnitsa/animallibrary/service/AbstractService.java
@@ -3,6 +3,7 @@ package dev.piatnitsa.animallibrary.service;
 import dev.piatnitsa.animallibrary.exception.ExceptionMessageCode;
 import dev.piatnitsa.animallibrary.exception.FieldError;
 import dev.piatnitsa.animallibrary.exception.NoSuchEntityException;
+import dev.piatnitsa.animallibrary.validator.IdentifiableValidator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public abstract class AbstractService<T> implements CRUDService<T> {
 
     @Override
     public T getById(long id) {
+        IdentifiableValidator.validateId(id);
         Optional<T> item = repository.findById(id);
         if (!item.isPresent()) {
             throw new NoSuchEntityException(new FieldError(ExceptionMessageCode.ENTITY_NOT_EXIST, id));
