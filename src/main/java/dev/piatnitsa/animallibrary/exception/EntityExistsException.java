2e3790191ec545d9f31ca757c8d9ff0dde233211
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-19 13:02:05 +0300

    Exception has been renamed

---
+++ src/main/java/dev/piatnitsa/animallibrary/exception/EntityExistsException.java
@@ -0,0 +1,18 @@
+package dev.piatnitsa.animallibrary.exception;
+
+public class EntityExistsException extends RuntimeException {
+    public EntityExistsException() {
+    }
+
+    public EntityExistsException(String message) {
+        super(message);
+    }
+
+    public EntityExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityExistsException(Throwable cause) {
+        super(cause);
+    }
+}
