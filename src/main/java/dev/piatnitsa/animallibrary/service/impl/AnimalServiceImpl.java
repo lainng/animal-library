951ac1bfa5114f380d4651f24de59b4dfc42ad2a
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-19 21:36:16 +0300

    Implemented insert() method

--- src/main/java/dev/piatnitsa/animallibrary/service/impl/AnimalServiceImpl.java
+++ src/main/java/dev/piatnitsa/animallibrary/service/impl/AnimalServiceImpl.java
@@ -1,11 +1,14 @@
 package dev.piatnitsa.animallibrary.service.impl;
 
-import dev.piatnitsa.animallibrary.exception.EntityAlreadyExistedException;
+import dev.piatnitsa.animallibrary.exception.EntityExistsException;
+import dev.piatnitsa.animallibrary.exception.ExceptionMessageCode;
 import dev.piatnitsa.animallibrary.model.Animal;
 import dev.piatnitsa.animallibrary.repository.AnimalRepository;
 import dev.piatnitsa.animallibrary.service.AbstractService;
 import dev.piatnitsa.animallibrary.service.AnimalService;
 import dev.piatnitsa.animallibrary.util.AnimalDataUpdater;
+import dev.piatnitsa.animallibrary.validator.AnimalValidator;
+import dev.piatnitsa.animallibrary.validator.IdentifiableValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,17 +27,28 @@ public class AnimalServiceImpl
     }
 
     @Override
+    public Animal insert(Animal newEntity) {
+        AnimalValidator.validate(newEntity);
+        return repository.save(newEntity);
+    }
+
+    @Override
     public Animal update(long id, Animal newDataEntity) {
         Animal currentAnimal = getById(id);
-        Optional<Animal> optionalAnimal = repository.findByNickname(currentAnimal.getNickname());
-        if (optionalAnimal.isPresent()) {
-            throw new EntityAlreadyExistedException();
+        AnimalValidator.validateForUpdate(newDataEntity);
+        if (newDataEntity.getNickname() != null) {
+            Optional<Animal> optionalAnimal = repository.findByNickname(newDataEntity.getNickname());
+            if (optionalAnimal.isPresent()) {
+                throw new EntityExistsException(ExceptionMessageCode.ANIMAL_ALREADY_EXISTS);
+            }
         }
-        return AnimalDataUpdater.updateData(currentAnimal, newDataEntity);
+        AnimalDataUpdater.updateData(currentAnimal, newDataEntity);
+        return repository.save(currentAnimal);
     }
 
     @Override
     public void delete(long id) {
+        IdentifiableValidator.validateId(id);
         Animal deletedAnimal = getById(id);
         repository.delete(deletedAnimal);
     }
