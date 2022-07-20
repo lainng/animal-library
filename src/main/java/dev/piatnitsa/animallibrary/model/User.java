b751c5da95a8d9fedc0d0418d138b7981c3f7aa8
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-20 12:57:26 +0300

    Minor changes

--- src/main/java/dev/piatnitsa/animallibrary/model/User.java
+++ src/main/java/dev/piatnitsa/animallibrary/model/User.java
@@ -1,11 +1,9 @@
 package dev.piatnitsa.animallibrary.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "user")
 public class User {
 
     @Id
