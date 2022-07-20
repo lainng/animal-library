30a8cb5567eb8a21878cf6de8fc15c72d01a9aec
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-20 22:21:35 +0300

    Fixed missing password encoder

--- src/main/java/dev/piatnitsa/animallibrary/service/impl/UserServiceImpl.java
+++ src/main/java/dev/piatnitsa/animallibrary/service/impl/UserServiceImpl.java
@@ -6,21 +6,26 @@ import dev.piatnitsa.animallibrary.service.AbstractService;
 import dev.piatnitsa.animallibrary.service.UserService;
 import dev.piatnitsa.animallibrary.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl extends AbstractService<User> implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder) {
         super(userRepository);
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User insert(User newEntity) {
         UserValidator.validate(newEntity);
+        newEntity.setPassword(passwordEncoder.encode(newEntity.getPassword()));
         return userRepository.save(newEntity);
     }
 
