00013259fb1fe414ef7b68156bf88ac5f40be313
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-20 22:21:53 +0300

    Added new exception message codes

--- src/main/java/dev/piatnitsa/animallibrary/exception/ExceptionMessageCode.java
+++ src/main/java/dev/piatnitsa/animallibrary/exception/ExceptionMessageCode.java
@@ -8,4 +8,10 @@ public class ExceptionMessageCode {
     public static final String USER_NOT_EXIST = "user.notExist";
 
     public static final String ENTITY_NOT_EXIST = "entity.noSuchEntity";
+    public static final String ENTITY_BAD_ID = "entity.badId";
+
+    public static final String ANIMAL_BAD_NICKNAME = "animal.badNickname";
+    public static final String ANIMAL_BAD_DATE_OF_BIRTH = "animal.badDateOfBirth";
+    public static final String ANIMAL_NOT_EXIST = "animal.notExist";
+    public static final String ANIMAL_ALREADY_EXISTS = "animal.alreadyExists";
 }
