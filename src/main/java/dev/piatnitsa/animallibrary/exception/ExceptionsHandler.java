76a69ac752343a4b7c620a136473640b89046e51
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-26 21:05:14 +0300

    Fixed incorrect fetching message from the bundle

--- src/main/java/dev/piatnitsa/animallibrary/exception/ExceptionsHandler.java
+++ src/main/java/dev/piatnitsa/animallibrary/exception/ExceptionsHandler.java
@@ -2,6 +2,7 @@ package dev.piatnitsa.animallibrary.exception;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -63,7 +64,7 @@ public class ExceptionsHandler {
         String details = messageSource.getMessage(
                 fieldError.getMessageCode(),
                 null,
-                Locale.getDefault()
+                LocaleContextHolder.getLocale()
         );
         return String.format(details, fieldError.getErrorValue());
     }
