76d01c9fd46690e95785d510a32455d48c108bf4
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-07-29 15:28:16 +0300

    Added equals and hashcode methods

--- src/main/java/dev/piatnitsa/animallibrary/exception/FieldError.java
+++ src/main/java/dev/piatnitsa/animallibrary/exception/FieldError.java
@@ -1,5 +1,7 @@
 package dev.piatnitsa.animallibrary.exception;
 
+import java.util.Objects;
+
 /**
  * This class is an entity that contains an exception message code and an invalid field value.
  * Required for entity validation.
@@ -35,5 +37,18 @@ public class FieldError {
 
     public void setErrorValue(Object errorValue) {
         this.errorValue = errorValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FieldError error = (FieldError) o;
+        return Objects.equals(messageCode, error.messageCode) && Objects.equals(errorValue, error.errorValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(messageCode, errorValue);
     }
 }
